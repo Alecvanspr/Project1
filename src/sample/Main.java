6cbbda2e68a2642ae85112f987f78c532330fa2f
Replaz1 <57253719+Replaz1@users.noreply.github.com>
2020-05-04 21:20:59 +0200

    begin gemaakt met loginscene en homescene

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,51 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Scene loginScene, homeScene;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        //Login screen stuff
+        Label lblUserName = new Label("Username");
+        Label lblPassWord = new Label("Password");
+        Button btnLogin = new Button("Log in");
+        btnLogin.setOnAction(e -> primaryStage.setScene(homeScene));
+        TextField textFieldUserName = new TextField();
+        PasswordField passwordField = new PasswordField();
+        Pane login = new Pane();
+        login.getChildren().addAll(lblUserName,lblPassWord,btnLogin,textFieldUserName,passwordField);
+        lblUserName.relocate(100,50);
+        textFieldUserName.relocate(100,70);
+        lblPassWord.relocate(100,95);
+        passwordField.relocate(100,115);
+        btnLogin.relocate(100,145);
+
+        loginScene = new Scene(login,800,600);
+
+        Label welcome = new Label("Welcome to the homescreen");
+        Pane home = new Pane();
+        home.getChildren().add(welcome);
+        welcome.relocate(400,300);
+
+        homeScene = new Scene(home,800,600);
+
+        primaryStage.setScene(loginScene);
+        primaryStage.setTitle("Title here");
         primaryStage.show();
     }
 
