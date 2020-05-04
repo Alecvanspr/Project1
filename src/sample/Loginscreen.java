67efbfa36eafc66ec34a2a54395443f6d7bdfae3
alecv <Alecvanspr@gmail.com>
2020-05-04 16:10:17 +0200

    completo

--- src/sample/Loginscreen.java
+++ src/sample/Loginscreen.java
@@ -1,14 +1,4 @@
 package sample;
 
 public class Loginscreen {
-    public Loginscreen(){
-
-    }
-
-    /*
-    hier komt de code voor het inlogscherm.
-    deze laat ik dan activeren op het main scherm.
-    jullie moeten ook een class aanmaken voor de marktplaats app
-    note: het moet in het engels.
-     */
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,99 +1,19 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
+
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-
 public class Main extends Application {
-    ArrayKeeper arraykeeper = new ArrayKeeper();
-    public Stage window;
-    public Pane login = new Pane();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Login screen stuff
-        Label lblUserName = new Label("Username");
-        Label lblPassWord = new Label("Password");
-        Button btnLogin = new Button("Log in");
-        Button btnSignUp = new Button("Create an account");
-
-        delettis();
-
-        TextField textFieldUserName = new TextField();
-        PasswordField passwordField = new PasswordField();
-        login.getChildren().addAll(lblUserName,lblPassWord,btnLogin,textFieldUserName,passwordField,btnSignUp);
-        lblUserName.relocate(100,50);
-        textFieldUserName.relocate(100,70);
-        lblPassWord.relocate(100,95);
-        passwordField.relocate(100,115);
-        btnLogin.relocate(100,145);
-        btnSignUp.relocate(150,145);
-
-        btnLogin.setOnAction(e -> {
-            if(gegevensCheck(passwordField.getText(),(textFieldUserName.getText()))){
-                Homescreen home = new Homescreen();
-                try {
-                    home.start(primaryStage);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }else{
-                Label error = new Label("Sorry, try again");
-                login.getChildren().add(error);
-                error.relocate(400,400);
-            }
-        });
-        btnSignUp.setOnAction(e -> {
-            SignUpScreen signUpScreen = new SignUpScreen();
-            try{
-                signUpScreen.start(primaryStage);
-            } catch (Exception ex){
-                ex.printStackTrace();
-            }
-        });
-
-
-        login.setMinSize(800,600);
-
-        Scene loginScene = new Scene(login,800,600);
-        primaryStage.setScene(loginScene);
-        primaryStage.setTitle("Log in");
-        window = primaryStage;
+        Pane root = new Pane();
+        primaryStage.setTitle("zeroXess");
+        primaryStage.setScene(new Scene(root, 600, 800));
         primaryStage.show();
-    }
-
-    public boolean gegevensCheck(String password,String username){
-        boolean ret = false;
-        int s = 0;
-        for(int i = 0;i<arraykeeper.Data.size();i++){
-            if(arraykeeper.Data.get(i).getName().equals(username)){
-                if(arraykeeper.Data.get(i).getPassword().equals(password)){
-                    ret = true;
-                    s = i;
-                }
-            }
-        }
-        arraykeeper.setCurrentUser(s);
-        return ret;
-    }
-    public void delettis(){
-        arraykeeper.SignUpData("Admin","Admin","Yesterday"); //deze manier werkt
-        PersonalData Admin = new PersonalData();
-        Admin.setName("admin");
-        Admin.setPassword("admin");
-        Admin.setBirthDate("admin");
-        arraykeeper.Data.add(Admin); //deze manier werkt
     }
     public static void main(String[] args) {
         launch(args);
