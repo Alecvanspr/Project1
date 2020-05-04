ee134cf6f8c6b7ce12f35cb82579611bd2270096
alecv <Alecvanspr@gmail.com>
2020-05-04 16:10:17 +0200

    completo

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,22 +1,20 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+
 import javafx.scene.Scene;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Pane root = new Pane();
+        primaryStage.setTitle("zeroXess");
+        primaryStage.setScene(new Scene(root, 600, 800));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
