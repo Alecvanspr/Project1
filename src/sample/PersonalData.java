a6a7a1dfebf66281726038d5fe0119efb9314784
Replaz1 <57253719+Replaz1@users.noreply.github.com>
2020-05-05 14:39:26 +0200

    personaldata

--- src/sample/PersonalData.java
+++ src/sample/PersonalData.java
@@ -2,7 +2,6 @@ package sample;
 
 public class PersonalData {
     private String name;
-    private String password;
     private String birthDate;
     private String phoneNumber;
     private String email;
@@ -18,12 +17,6 @@ public class PersonalData {
         return name;
     }
 
-    public void setPassword(String password){
-        this.password = password;
-    }
-    public String getPassword(){
-        return password;
-    }
     public void setBirthDate(String birthDate) {
         this.birthDate = birthDate;
     }
