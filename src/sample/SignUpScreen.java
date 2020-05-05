d50e15fdf2bc906576fe957a95b4fb789970d3a7
Replaz1 <57253719+Replaz1@users.noreply.github.com>
2020-05-05 14:26:25 +0200

    hahahah leuke opdracht je kan nu naar elk scherm

--- src/sample/SignUpScreen.java
+++ src/sample/SignUpScreen.java
@@ -11,21 +11,18 @@ import javafx.stage.Stage;
 
 public class SignUpScreen extends Application {
     Scene SignUpp;
-    Main main = new Main();
     public void start(Stage stage) throws Exception{
         Label lblUserName = new Label("Username");
         Label lblPassWord = new Label("Password");
         Label lblPassWordConf = new Label("Confirm password");
-        Label lblBirthdate = new Label("Birth date");
         TextField textFieldUserName = new TextField();
         PasswordField passwordField = new PasswordField();
         PasswordField passwordFieldConf = new PasswordField();
-        TextField textFieldBirth = new TextField();
         Button btnRegister = new Button("Register");
         Pane register = new Pane();
 
         register.getChildren().addAll(lblUserName,lblPassWord,lblPassWordConf
-                , textFieldUserName, passwordField, passwordFieldConf,textFieldBirth,lblBirthdate, btnRegister);
+        , textFieldUserName, passwordField, passwordFieldConf, btnRegister);
 
         lblUserName.relocate(100,50);
         textFieldUserName.relocate(100,70);
@@ -33,32 +30,7 @@ public class SignUpScreen extends Application {
         passwordField.relocate(100,115);
         lblPassWordConf.relocate(100, 140);
         passwordFieldConf.relocate(100,160);
-        lblBirthdate.relocate(100,185);
-        textFieldBirth.relocate(100,205);
-        btnRegister.relocate(100,235);
-
-        //dit zo een method maken
-        btnRegister.setOnAction(e->{
-            if((!(passwordField.getText().equals("")))&&(!(textFieldUserName.getText().equals("")))){
-                System.out.println("Waar");
-                if(passwordField.getText().equals(passwordFieldConf.getText())) {
-                    main.arraykeeper.SignUpData(textFieldUserName.getText(),passwordField.getText(),textFieldBirth.getText());
-                    try{
-                        main.start(stage);
-                    } catch (Exception ex){
-                        ex.printStackTrace();
-                    }
-                }else {
-                    Label passwordWrong = new Label("Passwords don't match");
-                    passwordWrong.relocate(100,265);
-                    register.getChildren().add(passwordWrong);
-                }
-            }else{
-                    Label emptyFields = new Label("Fields are empty");
-                    emptyFields.relocate(100,265);
-                    register.getChildren().add(emptyFields);
-            }
-        });
+        btnRegister.relocate(100,190);
 
         SignUpp = new Scene(register, 800,600);
         stage.setScene(SignUpp);
