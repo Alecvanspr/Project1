b98bcb522147c8aa189c94957aea72d9ab9a4b9f
Jvorwerk75 <17047242@student.hhs.nl>
2020-05-14 18:36:10 +0200

    Iets veranderd aan AuctionList title en button

--- src/sample/market/AuctionList.java
+++ src/sample/market/AuctionList.java
@@ -10,7 +10,50 @@ import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.text.Font;
 import sample.Homescreen;
-public class AuctionList extends Application {
+public class AuctionList{
     Scene AuctionList;
     Marketplace marketplace = new Marketplace();
+
+    public void start(Stage stage) throws Exception{
+        Button btnBack = new Button("Back");
+        btnBack.relocate(10, 565);
+        Pane auctionList = new Pane();
+        auctionList.getChildren().add(btnBack);
+        Label startLabel = new Label("Here you can see al the auctions");
+        startLabel.setFont(Font.font("Arial",30));
+        startLabel.relocate(200,0);
+        auctionList.getChildren().add(startLabel);
+
+        btnBack.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                btnBack.setScaleX(1.5);
+                btnBack.setScaleY(1.5);
+            }
+        });
+        btnBack.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                btnBack.setScaleX(1.0);
+                btnBack.setScaleY(1.0);
+            }
+        });
+
+        btnBack.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                Marketplace markerplace = new Marketplace();
+                try {
+                    markerplace.start(stage);
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        AuctionList = new Scene(auctionList, 800, 600);
+        stage.setTitle("Auction list");
+        stage.setScene(AuctionList);
+        stage.show();
+    }
 }
