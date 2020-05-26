6d9bb1975d7a2d3c7fe5b3cd55c08a364878cd35
JasperPhilippi <19008279@student.hhs.nl>
2020-05-26 17:40:48 +0200

    scuffed tekenen enzo

--- src/sample/educative/WriteScreen.java
+++ src/sample/educative/WriteScreen.java
@@ -2,26 +2,35 @@ package sample.educative;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.util.Random;
 
 public class WriteScreen  extends Application {
     Pane pane = new Pane();
-    Scene scene;
+    Scene scene = new Scene(pane, 800, 600);
     GetImage getImage = new GetImage();
+    Canvas canvas = new Canvas(800,600);
+    GraphicsContext gc;
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        drawing();
+
         Random random = new Random();
-        int getRandomLetter = random.nextInt(53);
-        Image displayLetter = new Image(getImage.getImage(getRandomLetter));
+        int getRandomLetter = random.nextInt(getImage.letterImages.size()-1);
+        BackgroundImage displayLetter = new BackgroundImage(new Image(getImage.letterImages.get(getRandomLetter)),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
         ImageView imageView = new ImageView();
-        imageView.setImage(displayLetter);
+        pane.setBackground(new Background(displayLetter));
         Button btnBack = new Button("back");
         btnBack.relocate(0,575);
 
@@ -29,8 +38,7 @@ public class WriteScreen  extends Application {
             goEducativeScreen(stage);
         });
 
-        pane.getChildren().addAll(imageView,btnBack);
-        scene = new Scene(pane, 800, 600);
+        pane.getChildren().addAll(canvas, imageView,btnBack);
         stage.setTitle("Write Screen");
         stage.setScene(scene);
         stage.show();
@@ -42,5 +50,21 @@ public class WriteScreen  extends Application {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+    public void drawing(){
+        gc = canvas.getGraphicsContext2D();
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(12);
+
+        scene.setOnMousePressed(e->{
+            gc.beginPath();
+            gc.lineTo(e.getSceneX(), e.getSceneY());
+            gc.stroke();
+        });
+
+        scene.setOnMouseDragged(e ->{
+            gc.lineTo(e.getSceneX(), e.getSceneY());
+            gc.stroke();
+        });
     }
 }
