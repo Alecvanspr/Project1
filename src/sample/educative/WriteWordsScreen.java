57986490cb5e7b366bee51ef58b082f8a2254a5e
JasperPhilippi <19008279@student.hhs.nl>
2020-05-28 10:03:10 +0200

    hahahaha funny picture xddd

--- src/sample/educative/WriteWordsScreen.java
+++ src/sample/educative/WriteWordsScreen.java
@@ -14,25 +14,27 @@ public class WriteWordsScreen extends Application {
 
     Pane pane = new Pane();
     Scene scene = new Scene(pane, 800, 600);
-    Image image;
-    ImageView imageView;
     WriteWordsCards writeWordsCards = new WriteWordsCards();
     GetImage getImage = new GetImage();
-    Random random = new Random();
-    int rng = random.nextInt(writeWordsCards.imageFlashcards.size() - 1);
+    ImageView iv;
+    Image image;
 
     @Override
     public void start(Stage stage) throws Exception {
 
         makeBackGround();
         Button btnBack = new Button("back");
+        Button btnNewPic = new Button("new thing");
         btnBack.relocate(0, 575);
         btnBack.setOnAction(E -> {
             goBack(stage);
         });
+        btnNewPic.setOnAction(e->{
+            makeBackGround();
+        });
 
 
-        pane.getChildren().addAll(btnBack);
+        pane.getChildren().addAll(btnBack, btnNewPic);
 
         stage.setTitle("Write words screen");
         stage.setScene(scene);
@@ -49,9 +51,15 @@ public class WriteWordsScreen extends Application {
     }
 
     public void makeBackGround() {
+        pane.getChildren().remove(iv);
+        Random random = new Random();
+        int rng = random.nextInt(writeWordsCards.imageFlashcards.size() - 1);
         image = writeWordsCards.imageFlashcards.get(rng).getImage();
-        ImageView iv = new ImageView();
+        iv = new ImageView();
         iv.setImage(image);
+        iv.setPreserveRatio(true);
+        iv.setFitHeight(300);
+        iv.relocate(350,250);
         pane.getChildren().add(iv);
     }
 }
