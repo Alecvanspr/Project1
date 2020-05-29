53b369856d51777eac66c0f5cb15632ad30a9f3c
noeger <benjamin.claudio@gmail.com>
2020-05-29 03:31:06 +0200

    Random game versie 2

--- src/sample/educative/MathGame/MathScreen.java
+++ src/sample/educative/MathGame/MathScreen.java
@@ -18,7 +18,7 @@ import java.util.Random;
 public class MathScreen  extends Application {
     Pane pane = new Pane();
     Scene scene;
-
+    MathGame mathGame = new MathGame();
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -28,29 +28,68 @@ public class MathScreen  extends Application {
             goEducativeScreen(stage);
         });
 
-        Button btnAnswer1 = new Button(Integer.toString(MathGame.ButtonNumber0()));
-        Button btnAnswer2 = new Button(Integer.toString(MathGame.ButtonNumber1()));
-        Button btnAnswer3 = new Button(Integer.toString(MathGame.ButtonNumber2()));
-        Button btnAnswer4 = new Button(Integer.toString(MathGame.ButtonNumber3()));
+        Button btnStart = new Button("Start");
+        Button btnAnswer1 = new Button("");
+        Button btnAnswer2 = new Button("");
+        Button btnAnswer3 = new Button("");
+        Button btnAnswer4 = new Button("");
 
-        Label lblMathProblem = new Label("test");
+
+        Label lblMathProblem = new Label("");
         lblMathProblem.setFont(Font.font("Arial",50));
         lblMathProblem.relocate(295, 100);
-        lblMathProblem.setText(MathGame.MathQuestion());
 
-        pane.getChildren().addAll(btnBack,btnAnswer1,btnAnswer2,btnAnswer3,btnAnswer4, lblMathProblem);
+        Label lblWrongAnswer = new Label("Wrong Aswers");
+        lblWrongAnswer.setFont(Font.font("Arial",25));
+        lblWrongAnswer.relocate(30, 200);
+
+        Label lblWrongAnswerTotal = new Label("0");
+        lblWrongAnswerTotal.setFont(Font.font("Arial",25));
+        lblWrongAnswerTotal.relocate(110, 230);
+
+        pane.getChildren().addAll(btnBack,btnAnswer1,btnAnswer2,btnAnswer3,btnAnswer4, lblMathProblem,btnStart,lblWrongAnswer,lblWrongAnswerTotal);
         scene = new Scene(pane, 800, 600);
         stage.setTitle("Math screen");
         stage.setScene(scene);
         stage.show();
 
 
+        btnStart.relocate(390,575);
+        btnStart.setOnMouseClicked(e -> {
+            if(!mathGame.getStartBtn()) {
+                lblMathProblem.setText(mathGame.MathQuestion());
+                btnAnswer1.setText(Integer.toString(mathGame.ButtonNumber0()));
+                btnAnswer2.setText(Integer.toString(mathGame.ButtonNumber1()));
+                btnAnswer3.setText(Integer.toString(mathGame.ButtonNumber2()));
+                btnAnswer4.setText(Integer.toString(mathGame.ButtonNumber3()));
+                mathGame.setRandNumb();
+                mathGame.setGame(true);
+            }
+        });
 
         setButtonLayout(btnAnswer1);
         btnAnswer1.relocate(225,200);
         btnAnswer1.setOnMouseClicked(e -> {
-            MathGame.NextNumber();
+            if(mathGame.getGame()) {
+                lblMathProblem.setText(mathGame.MathQuestion());
+                btnAnswer1.setText(Integer.toString(mathGame.ButtonNumber0()));
+                btnAnswer2.setText(Integer.toString(mathGame.ButtonNumber1()));
+                btnAnswer3.setText(Integer.toString(mathGame.ButtonNumber2()));
+                btnAnswer4.setText(Integer.toString(mathGame.ButtonNumber3()));
+                mathGame.setRandNumb();
+            }
         });
+        btnAnswer1.setOnMousePressed(e -> {
+            if(mathGame.getAnswer() == mathGame.ButtonNumber1()) {
+                btnAnswer1.setStyle("-fx-background-color: #00ff00; ");
+                lblWrongAnswerTotal.setText(Integer.toString(mathGame.getWrongAnswers()));
+
+            }else{
+                btnAnswer1.setStyle("-fx-background-color: #ff0000; ");
+                lblWrongAnswerTotal.setText(Integer.toString(mathGame.getWrongAnswers()));
+            }
+        });
+
         btnAnswer1.setOnMouseExited(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
@@ -64,12 +103,19 @@ public class MathScreen  extends Application {
             }
         });
 
-        //Contact button
         setButtonLayout(btnAnswer2 );
         btnAnswer2 .relocate(450,200);
         btnAnswer2 .setOnAction(e -> {
-            MathGame.NextNumber();
+            if(mathGame.getGame()) {
+                lblMathProblem.setText(mathGame.MathQuestion());
+                btnAnswer1.setText(Integer.toString(mathGame.ButtonNumber0()));
+                btnAnswer2.setText(Integer.toString(mathGame.ButtonNumber1()));
+                btnAnswer3.setText(Integer.toString(mathGame.ButtonNumber2()));
+                btnAnswer4.setText(Integer.toString(mathGame.ButtonNumber3()));
+                mathGame.setRandNumb();
+            }
         });
+
         btnAnswer2 .setOnMouseEntered(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
@@ -83,11 +129,17 @@ public class MathScreen  extends Application {
             }
         });
 
-        //Market button
         setButtonLayout(btnAnswer3);
         btnAnswer3.relocate(225,400);
         btnAnswer3.setOnAction(e ->{
-            MathGame.NextNumber();
+            if(mathGame.getGame()) {
+                lblMathProblem.setText(mathGame.MathQuestion());
+                btnAnswer1.setText(Integer.toString(mathGame.ButtonNumber0()));
+                btnAnswer2.setText(Integer.toString(mathGame.ButtonNumber1()));
+                btnAnswer3.setText(Integer.toString(mathGame.ButtonNumber2()));
+                btnAnswer4.setText(Integer.toString(mathGame.ButtonNumber3()));
+                mathGame.setRandNumb();
+            }
         });
         btnAnswer3.setOnMouseExited(new EventHandler<MouseEvent>() {
             @Override
@@ -102,11 +154,17 @@ public class MathScreen  extends Application {
             }
         });
 
-        //Livestoch button
         setButtonLayout(btnAnswer4);
         btnAnswer4.relocate(450,400);
         btnAnswer4.setOnAction(e -> {
-            MathGame.NextNumber();
+            if(mathGame.getGame()) {
+                lblMathProblem.setText(mathGame.MathQuestion());
+                btnAnswer1.setText(Integer.toString(mathGame.ButtonNumber0()));
+                btnAnswer2.setText(Integer.toString(mathGame.ButtonNumber1()));
+                btnAnswer3.setText(Integer.toString(mathGame.ButtonNumber2()));
+                btnAnswer4.setText(Integer.toString(mathGame.ButtonNumber3()));
+                mathGame.setRandNumb();
+            }
         });
         btnAnswer4.setOnMouseEntered(new EventHandler<MouseEvent>() {
             @Override
