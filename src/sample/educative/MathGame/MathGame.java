90a36ba17c53897aa5c6adc936c5db2f59f4d6fc
noeger <benjamin.claudio@gmail.com>
2020-05-29 03:31:06 +0200

    Random game versie 2

--- src/sample/educative/MathGame/MathGame.java
+++ src/sample/educative/MathGame/MathGame.java
@@ -3,59 +3,93 @@ package sample.educative.MathGame;
 import java.util.Random;
 
 public class MathGame {
-    static Random random = new Random();
-    static int randNumb1 = random.nextInt(50);
-    static int randNumb2 = random.nextInt(50);
-    static int randButNumb = random.nextInt(3);
+    private Random random = new Random();
+    private int randNumb1 = random.nextInt(50);
+    private int randNumb2 = random.nextInt(50);
+    private int randButNumb = random.nextInt(3);
 
-    Boolean game = true;
-    int wrongAnswers = 0;
-    static int answer = randNumb1 + randNumb2;
-    static int randAnswer1 = random.nextInt(50);
-    static int randAnswer2 = random.nextInt(50);
-    static int randAnswer3 = random.nextInt(50);
-    static int randAnswer4 = random.nextInt(50);
+    private Boolean game = false;
+    private Boolean startBtn = false;
+    private int wrongAnswers = 0;
+    private int randAnswer1 = random.nextInt(50);
+    private int randAnswer2 = random.nextInt(50);
+    private int randAnswer3 = random.nextInt(50);
+    private int randAnswer4 = random.nextInt(50);
+    private int answer = randNumb1 + randNumb2;
 
-    public static String MathQuestion(){
-        return randNumb1 + " + " + randNumb2 + " = ?";
+    public void setGame(Boolean game) {
+        this.game = game;
+        this.startBtn = true;
     }
-    public static int ButtonNumber1(){
+
+    public int ButtonNumber1(){
         if(randButNumb == 0) {
             return answer;
         }else{
             return randAnswer1;
         }
     }
-    public static int ButtonNumber2(){
+    public int ButtonNumber2(){
         if(randButNumb == 1) {
             return answer;
         }else{
             return randAnswer2;
         }
     }
-    public static int ButtonNumber3(){
+    public int ButtonNumber3(){
         if(randButNumb == 2) {
             return answer;
         }else{
             return randAnswer3;
         }
     }
-    public static int ButtonNumber0(){
+    public int ButtonNumber0(){
         if(randButNumb == 3) {
             return answer;
         }else{
             return randAnswer4;
         }
     }
+    public void setRandNumb(){
+        this.randNumb1 = random.nextInt(50);
+        this.randNumb2 = random.nextInt(50);
+        this.randButNumb = random.nextInt(3);
+        this.randAnswer1 = random.nextInt(50);
+        this.randAnswer2 = random.nextInt(50);
+        this.randAnswer3 = random.nextInt(50);
+        this.randAnswer4 = random.nextInt(50);
+        this.answer = randNumb1 + randNumb2;
+    }
 
-    public static void NextNumber(){
-        randNumb1 = random.nextInt();
-        randNumb2 = random.nextInt();
-        randButNumb = random.nextInt();
-        randAnswer1 = random.nextInt();
-        randAnswer2 = random.nextInt();
-        randAnswer3 = random.nextInt();
-        randAnswer4 = random.nextInt();
+    public Boolean getGame() {
+        return game;
+    }
+
+    public Boolean getStartBtn() {
+        return startBtn;
+    }
+
+    public int getRandNumb1() {
+        return randNumb1;
+    }
+
+    public int getRandNumb2() {
+        return randNumb2;
+    }
+    public int getAnswer() {
+        return answer;
+    }
+
+    public int getWrongAnswers() {
+        return wrongAnswers;
+    }
+
+    public void setWrongAnswers(int wrongAnswers) {
+        this.wrongAnswers = wrongAnswers;
+    }
+
+    public String MathQuestion(){
+        return getRandNumb1() + " + " + getRandNumb2() + " = ?";
     }
 
     public void CheckAnswer(){
