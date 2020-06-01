0a0c5b430e856814f890c5dd5ffaebcde4c418c3
JasperPhilippi <19008279@student.hhs.nl>
2020-06-01 12:42:22 +0200

    hangman

--- src/sample/educative/read/HangmanScreen.java
+++ src/sample/educative/read/HangmanScreen.java
@@ -1,4 +1,37 @@
 package sample.educative.read;
 
-public class HangmanScreen {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class HangmanScreen extends Application {
+    Pane pane = new Pane();
+    Scene scene;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button btnBack = new Button("back");
+        btnBack.relocate(0,575);
+        btnBack.setOnAction(E->{
+            goEducativeScreen(stage);
+        });
+
+        pane.getChildren().addAll(btnBack);
+
+        scene = new Scene(pane, 800, 600);
+        stage.setTitle("Hangman screen");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void goEducativeScreen(Stage stage){
+        ReadScreen readScreen = new ReadScreen();
+        try {
+            readScreen.start(stage);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
