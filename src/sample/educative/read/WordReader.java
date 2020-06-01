bbb185a6c7eab29121e08d4428c01d6a2eb31774
JasperPhilippi <19008279@student.hhs.nl>
2020-06-01 13:06:26 +0200

    daadwerkelijk begin aan hangman

---
+++ src/sample/educative/read/WordReader.java
@@ -0,0 +1,30 @@
+package sample.educative.read;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class WordReader {
+    private static final String filename = "/_txtfiles/words.txt";
+
+    private ArrayList<String> words = new ArrayList<String>();
+
+    public WordReader(){
+        try(InputStream in = getClass().getResourceAsStream(filename);
+                BufferedReader bf = new BufferedReader(new InputStreamReader(in))){
+                    String line = "";
+                    while((line = bf.readLine()) != null)
+                        words.add(line);
+                }
+        catch(Exception e){
+            System.out.println("Couldn't find/read file: "+filename);
+            System.out.println("Error message: "+e.getMessage());
+        }
+    }
+
+    public String getRandomWord(){
+        if(words.isEmpty()) return "NO-DATA";
+        return words.get((int)(Math.random()*words.size()));
+    }
+}
