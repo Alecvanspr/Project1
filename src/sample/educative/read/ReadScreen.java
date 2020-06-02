29b28aa87cc0a1f9d75be0e28209e790432c76e8
JasperPhilippi <19008279@student.hhs.nl>
2020-06-02 11:55:03 +0200

    rondgespeeld met video, kan weg als het moet

--- src/sample/educative/read/ReadScreen.java
+++ src/sample/educative/read/ReadScreen.java
@@ -3,8 +3,6 @@ package sample.educative.read;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import sample.educative.EducativeHomeScreen;
@@ -16,8 +14,13 @@ public class ReadScreen  extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Button btnBack = new Button("back");
-        Button btnGrammar = new Button("grammer");
+        Button btnGrammar = new Button("grammar");
         Button btnHangman = new Button("Hangman");
+        Button btnYouCantSeeMe = new Button("you cant see mee");
+        btnYouCantSeeMe.relocate(300,350);
+        btnYouCantSeeMe.setOnAction(e->{
+            goVideo(stage);
+        });
         btnHangman.relocate(200,350);
         btnBack.relocate(0,575);
         btnGrammar.relocate(200,200);
@@ -31,7 +34,7 @@ public class ReadScreen  extends Application {
             goGrammer(stage);
         });
 
-        pane.getChildren().addAll(btnBack,btnHangman,btnGrammar);
+        pane.getChildren().addAll(btnBack,btnHangman,btnGrammar,btnYouCantSeeMe);
 
         scene = new Scene(pane, 800, 600);
         stage.setTitle("Read screen");
@@ -54,6 +57,16 @@ public class ReadScreen  extends Application {
             ex.printStackTrace();
         }
     }
+
+    public void goVideo(Stage stage) {
+        VideoTest videoTest = new VideoTest();
+        try {
+            videoTest.start(stage);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
     public void goGrammer(Stage stage){
         GrammarScreen grammarScreen = new GrammarScreen();
         try {
---
+++ src/sample/educative/read/VideoTest.java
@@ -0,0 +1,51 @@
+package sample.educative.read;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+import sample.educative.EducativeHomeScreen;
+
+import java.io.File;
+
+public class VideoTest extends Application {
+    Pane pane = new Pane();
+    Scene scene = new Scene(pane,800,600);
+    MediaPlayer mediaPlayer;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button btnBack = new Button("back");
+        btnBack.relocate(0,575);
+        btnBack.setOnAction(E->{
+            goReadScreen(stage);
+            mediaPlayer.dispose();
+        });
+        String path = System.getProperty("user.dir");
+        File f = new File(path,"src/video/JohnCena.mp4");
+        Media media = new Media(f.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        MediaView mediaView = new MediaView(mediaPlayer);
+        mediaView.setPreserveRatio(true);
+        mediaView.setFitHeight(600);
+        mediaPlayer.setAutoPlay(true);
+        mediaView.relocate(250,0);
+        pane.getChildren().addAll(mediaView, btnBack);
+        stage.setScene(scene);
+        stage.setTitle("JOHN CENA");
+        stage.show();
+    }
+
+    public void goReadScreen(Stage stage){
+        ReadScreen readScreen = new ReadScreen();
+        try {
+            readScreen.start(stage);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
