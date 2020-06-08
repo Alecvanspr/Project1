86a2de7a1bb46f36ff1477bddf6f2f2fe3743a68
alecv <Alecvanspr@gmail.com>
2020-06-08 17:47:28 +0200

    langzaam bezig met code in te korten

--- src/sample/inlogScreen/SignUpScreen.java
+++ src/sample/inlogScreen/SignUpScreen.java
@@ -15,64 +15,74 @@ public class SignUpScreen extends Application {
     Main main = new Main();
     ButtonSettings buttonSettings = new ButtonSettings();
     GoToScreens goToScreens = new GoToScreens();
-    //    String question1, question2, question3;
+    Pane pane = new Pane();
+
+    ComboBox securityQuestions = new ComboBox();
+    TextField textFieldUserName = new TextField();
+    PasswordField passwordField = new PasswordField();
+    PasswordField passwordFieldConf = new PasswordField();
+    TextField textFieldBirth = new TextField();
+    TextField securityAnswer = new TextField();
+
     public void start(Stage stage) throws Exception{
-        Button btnBack = new Button("Back");
+        makeTextFields();
+        makeLabels();
+        makeButtons(stage);
+        fin(stage);
+    }
+    public void fin(Stage stage){
+        SignUpp = new Scene(pane, 800,600);
+        stage.setTitle("Sign up");
+        stage.setScene(SignUpp);
+        stage.show();
+    }
+    public void makeTextFields(){
+        pane.getChildren().addAll(textFieldUserName, passwordField, passwordFieldConf,textFieldBirth,
+                securityQuestions, securityAnswer);
+        textFieldUserName.relocate(100,70);
+        passwordField.relocate(100,115);
+        passwordFieldConf.relocate(100,160);
+        textFieldBirth.relocate(100,205);
+        securityQuestions.relocate(100,250);
+        securityAnswer.relocate(100, 278);
+    }
+    public void makeLabels(){
         Label lblUserName = new Label("Username");
         Label lblPassWord = new Label("Password");
         Label lblPassWordConf = new Label("Confirm password");
         Label lblBirthdate = new Label("Birth date");
         Label lblSecurity = new Label("Security question in case you forget your password");
-        ComboBox securityQuestions = new ComboBox();
         securityQuestions.getItems().addAll("What is your favorite colour?",
                 "What was the name of your first pet?",
                 "What was your first city of residence?");
-        TextField securityAnswer = new TextField();
-        CheckBox checkBox = new CheckBox();
-
-        TextField textFieldUserName = new TextField();
-        PasswordField passwordField = new PasswordField();
-        PasswordField passwordFieldConf = new PasswordField();
-        TextField textFieldBirth = new TextField();
-
-        Button btnRegister = new Button("Register");
-        Pane register = new Pane();
-
-        register.getChildren().addAll(lblUserName,lblPassWord,lblPassWordConf
-                , textFieldUserName, passwordField, passwordFieldConf,textFieldBirth,lblBirthdate,
-                securityQuestions, securityAnswer, lblSecurity,btnRegister,btnBack);
-
         lblUserName.relocate(100,50);
-        textFieldUserName.relocate(100,70);
-        lblPassWord.relocate(100,95);
-        passwordField.relocate(100,115);
-        lblPassWordConf.relocate(100, 140);
-        passwordFieldConf.relocate(100,160);
-        lblBirthdate.relocate(100,185);
-        textFieldBirth.relocate(100,205);
         lblSecurity.relocate(100,230);
-        securityQuestions.relocate(100,250);
-        securityAnswer.relocate(100, 278);
-        btnRegister.relocate(100,320);
+        lblPassWord.relocate(100,95);
+        lblBirthdate.relocate(100,185);
+        lblPassWordConf.relocate(100, 140);
+        pane.getChildren().addAll(lblUserName,lblPassWord,lblPassWordConf,lblBirthdate,lblSecurity);
+    }
+    public void makeButtons(Stage stage){
+        makeBtnRegister(stage);
+    }
+    public void makeBtnBack(Stage stage){
+        Button btnBack = new Button("Back");
         btnBack.relocate(0, 570);
-
-        btnRegister.setOnAction(e->{
-            register(stage,register,passwordField.getText(),passwordFieldConf.getText(),textFieldUserName.getText(),
-                    textFieldBirth.getText(),securityAnswer.getText(),securityQuestions.getSelectionModel().getSelectedItem().toString());
-        });
         btnBack.setOnAction(e -> { //dit wordt zo een OK knop.
             goToScreens.goMain(stage);
         });
-
         buttonSettings.onMouse(btnBack);
+        pane.getChildren().add(btnBack);
+    }
+    public void makeBtnRegister(Stage stage){
+        Button btnRegister = new Button("Register");
+        btnRegister.setOnAction(e->{
+            register(stage,pane,passwordField.getText(),passwordFieldConf.getText(),textFieldUserName.getText(),
+                    textFieldBirth.getText(),securityAnswer.getText(),securityQuestions.getSelectionModel().getSelectedItem().toString());
+        });
         buttonSettings.onMouse(btnRegister);
-
-
-        register.getChildren().addAll(checkBox);
-        SignUpp = new Scene(register, 800,600);
-        stage.setTitle("Sign up");
-        stage.setScene(SignUpp);
-        stage.show();
+        btnRegister.relocate(100,320);
+        pane.getChildren().add(btnRegister);
     }
 
     //deze laat ik er in omdat het een javaFX class is.
