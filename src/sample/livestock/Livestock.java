33840a2b91c8fdb7f6a39d5dd23ac381901a1b12
alecv <Alecvanspr@gmail.com>
2020-06-08 12:46:39 +0200

    langzaam bezig met code in te korten

--- src/sample/livestock/Livestock.java
+++ src/sample/livestock/Livestock.java
@@ -33,56 +33,34 @@ public class Livestock extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
-        Button btnBack = new Button("Back");
-        Button btnAdd = new Button("Add animal");
+        makeLabels();
+        stage.setResizable(false);
+        makeBtnBack(stage);
+        makeBtnAdd(stage);
+        displayHealth(stage);
+        displayWeight(stage);
+        displayAllAnimals(stage);
+        makeToMakeAuction(stage);
+        makeMakeAuction(stage);
+        fin(stage);
+    }
+    public void fin(Stage stage){
+        liveStockScroll.setContent(liveStockPane);
+        stockScene = new Scene(liveStockScroll,800,600);
+        stage.setTitle("Livestock");
+        stage.setScene(stockScene);
+        stage.show();
+    }
+    public void makeLabels(){
         Label lblName = new Label("Name Animal");
         Label lblHealth = new Label("Last health");
         Label lblWeight = new Label("Last Weight");
         lblName.relocate(100,40);
         lblHealth.relocate(300,40);
         lblWeight.relocate(500,40);
-
-        stage.setResizable(false);
-
-        btnAdd.relocate(100,10);
-
-
-        ScrollBar scrollBar= new ScrollBar();
-        scrollBar.setOrientation(Orientation.VERTICAL);
-        scrollBar.setMinHeight(600);
-        scrollBar.relocate(775,0);
-        ScrollPane scrollPane = new ScrollPane(scrollBar);
-
-        displayHealth(stage);
-        displayWeight(stage);
-        displayAllAnimals(stage);
-
-        btnAdd.setOnAction(E->{
-            goToScreens.goNewAnimal(stage);
-        });
-
-        liveStockPane.getChildren().addAll(btnBack,btnAdd,lblName,lblHealth,lblWeight);
-        btnBack.relocate(10,565);
-        btnBack.setOnAction(e -> {
-            goToScreens.goHomeScreen(stage);
-        });
-        buttonSettings.onMouse(btnBack);
-
-        //Button to go back to make auction
-        Button toMakeAuction = new Button("Go to Auctions");
-        toMakeAuction.relocate(575, 500);
-        toMakeAuction.setPrefWidth(150);
-        toMakeAuction.setPrefHeight(50);
-        liveStockPane.getChildren().add(toMakeAuction);
-        buttonSettings.onMouse(toMakeAuction);
-        toMakeAuction.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                goToScreens.goAutionlist(stage);
-            }
-        });
-
-        //Button to make Auction for your animals
+        liveStockPane.getChildren().addAll(lblName,lblHealth,lblWeight);
+    }
+    public void makeMakeAuction(Stage stage){
         Button makeAuction = new Button("Make Auction of animals");
         makeAuction.relocate(575, 400);
         makeAuction.setPrefWidth(150);
@@ -95,14 +73,21 @@ public class Livestock extends Application {
             }
         });
         buttonSettings.onMouse(makeAuction);
-
-        liveStockScroll.setContent(liveStockPane);
-        stockScene = new Scene(liveStockScroll,800,600);
-        stage.setTitle("Livestock");
-        stage.setScene(stockScene);
-        stage.show();
     }
-
+    public void makeToMakeAuction(Stage stage){
+        Button toMakeAuction = new Button("Go to Auctions");
+        toMakeAuction.relocate(575, 500);
+        toMakeAuction.setPrefWidth(150);
+        toMakeAuction.setPrefHeight(50);
+        liveStockPane.getChildren().add(toMakeAuction);
+        buttonSettings.onMouse(toMakeAuction);
+        toMakeAuction.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                goToScreens.goAutionlist(stage);
+            }
+        });
+    }
     public void displayAllAnimals(Stage stage){
         for(int i = 0; i<arrayKeeper.getPersonaldata().get(ArrayKeeper.getCurrentUser()).getAnimals().size(); i++){
             Label label = new Label(arrayKeeper.getPersonaldata().get(ArrayKeeper.getCurrentUser()).getAnimals().get(i).getName());
@@ -115,6 +100,22 @@ public class Livestock extends Application {
             liveStockPane.getChildren().add(label);
             setPlaceName(getPlaats() + 35);
         }
+    }
+    public void makeBtnBack(Stage stage){
+        Button btnBack = new Button("Back");
+        btnBack.relocate(10,565);
+        btnBack.setOnAction(e -> {
+            goToScreens.goHomeScreen(stage);
+        });
+        buttonSettings.onMouse(btnBack);
+        liveStockPane.getChildren().add(btnBack);
+    }
+    public void makeBtnAdd(Stage stage){
+        Button btnAdd = new Button("Add animal");
+        btnAdd.setOnAction(E->{
+            goToScreens.goNewAnimal(stage);
+        });
+        btnAdd.relocate(100,10);
     }
     public void displayHealth(Stage stage){
         for(int i = 0; i<arrayKeeper.getPersonaldata().get(ArrayKeeper.getCurrentUser()).getAnimals().size();i++){
