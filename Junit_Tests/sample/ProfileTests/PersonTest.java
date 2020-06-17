1a652891ac2586c75eadd8f13448637f76b062fe
alecv <Alecvanspr@gmail.com>
2020-06-17 20:20:35 +0200

    Tests aan het maken PersonalData.java

--- Junit_Tests/sample/ProfileTests/PersonTest.java
+++ Junit_Tests/sample/ProfileTests/PersonTest.java
@@ -22,19 +22,27 @@ public class PersonTest {
     public void testFirstPerson(){
         makeArraykeeper();
         testUserName(1,"legend27");
+        testName(1,"Henk de Steen");
         testPassword(1,"wachtwoord");
+        testBirthdate(1,"Yesterday");
+        testSecurityQuestion(1,"What you egg?");
+        testSecurityAnswer(1,"Stabs him");
+
     }
     @Test
     public void testSecondPerson(){
-        makeArraykeeper();
-        testUserName(2,"legend27");
-        testPassword(2,"wachtwoord");
+        testUserName(2,"Schilder123");
+        testName(2,"Henk de Schilder");
+        testPassword(2,"GooiInDeVerf");
+        testBirthdate(2,"Vandaag");
+        testSecurityQuestion(2,"Wat is je favorite kleur?");
+        testSecurityAnswer(2,"verf");
     }
     @Test
     public void testLastPerson(){
         makeArraykeeper();
-        testUserName(3,"legend27");
-        testPassword(3,"wachtwoord");
+        testUserName(3,"The_Chin");
+        testPassword(3,"Kin123");
     }
     public void testName(int place, String expected){
         Assert.assertEquals(ArrayKeeper.getData().get(place).getName(),expected);
@@ -45,7 +53,13 @@ public class PersonTest {
     public void testPassword(int place, String expected){
         Assert.assertEquals(ArrayKeeper.getData().get(place).getPassword(),expected);
     }
-    public void testBirthdate(int place,String expected){}
-    public void testSecurityQuestion(int place, String expected){}
-    public void textSecurityAnswer(int place, String expected){}
+    public void testBirthdate(int place,String expected){
+        Assert.assertEquals(ArrayKeeper.getData().get(place).getDateOfBirth(),expected);
+    }
+    public void testSecurityQuestion(int place, String expected){
+        Assert.assertEquals(ArrayKeeper.getData().get(place).getSecurityQuestion(),expected);
+    }
+    public void testSecurityAnswer(int place, String expected){
+        Assert.assertEquals(ArrayKeeper.getData().get(place).getSecurityAnswer(),expected);
+    }
 }
