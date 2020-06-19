fda5f0e2553c6d7e9badff97a4531e0c23193375
noeger <benjamin.claudio@gmail.com>
2020-06-20 01:40:10 +0200

    working 1 + test1

--- Junit_Tests/sample/educative/MathGame/MathGameTest.java
+++ Junit_Tests/sample/educative/MathGame/MathGameTest.java
@@ -1,12 +1,30 @@
 package sample.educative.MathGame;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MathGameTest {
-
+public class MathGameTest {
+    MathGame mathGame;
+    public void MakeNewMathGame(int operator){
+        mathGame= new MathGame();
+        mathGame.RandomOperator();
+        mathGame.setRandOperator(operator);
+    }
     @Test
-    void getAnswer() {
+    public void testRandomCorrect1(){
+        MakeNewMathGame(0);
+        Assert.assertEquals(mathGame.getRandNumb1()+mathGame.getRandNumb2(),mathGame.getAnswer());
+    }
+    @Test
+    public void testRandomCorrect2(){
+        MakeNewMathGame(0);
+        Assert.assertEquals(mathGame.getRandNumb1()+mathGame.getRandNumb2(),mathGame.getAnswer());
+    }
+    @Test
+    public void testRandomCorrect3(){
+        MakeNewMathGame(1);
+        Assert.assertEquals(mathGame.getRandNumb1()-mathGame.getRandNumb2(),mathGame.getAnswer());
     }
 }
---
+++ Junit_Tests/sample/educative/MathGame/MathScreenTest.java
@@ -0,0 +1,13 @@
+package sample.educative.MathGame;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathScreenTest {
+
+    @Test
+    void checkAnswer() {
+    }
+}
